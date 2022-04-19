8fd4fbfb8daeeae730335c12ceab748fa103521d
migdracios <migdracios@gmail.com>
2022-04-19 16:38:46 +0900

    project fan initial set

---
+++ projects/homework/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+@app.route('/fan')
+def home():
+    return render_template('fan.html')
+
+@app.route("/fan/post", methods=["POST"])
+def homework_post():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg':'POST 연결 완료!'})
+
+@app.route("/fan/get", methods=["GET"])
+def homework_get():
+    return jsonify({'msg':'GET 연결 완료!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
