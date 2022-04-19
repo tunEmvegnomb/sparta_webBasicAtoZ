08cf793aad6bc021b137a17eac5149e4465cb886
migdracios <migdracios@gmail.com>
2022-04-19 12:16:00 +0900

    POST API basic code

--- projects/prac/app.py
+++ projects/prac/app.py
@@ -14,5 +14,11 @@ def test_get():
     print(title_receive)
     return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
 
+@app.route('/test', methods=['POST'])
+def test_post():
+    title_receive = request.form['title_give']
+    print(title_receive)
+    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
