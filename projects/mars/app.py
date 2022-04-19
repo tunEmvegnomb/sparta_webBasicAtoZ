2a64810c53ca64622cf2d5f874dedfebfae948d2
migdracios <migdracios@gmail.com>
2022-04-19 12:38:28 +0900

    import pymongo with Mongo Cloud

--- projects/mars/app.py
+++ projects/mars/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.8yqbf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 
 @app.route('/mars')
 def home():
