232846c71873e40e7937c82003486e5c360b7a82
migdracios <migdracios@gmail.com>
2022-04-19 12:39:54 +0900

    fix GET API return result

--- projects/mars/app.py
+++ projects/mars/app.py
@@ -39,7 +39,7 @@ def web_mars_post():
 
 @app.route("/mars/get", methods=["GET"])
 def web_mars_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
+    return jsonify({'result': 'success', 'msg': 'GET 연결 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
