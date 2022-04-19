68a33a9dd86e7f6cad5a146b5bf32d6e0df79466
migdracios <migdracios@gmail.com>
2022-04-19 17:52:04 +0900

    fix index href bucket

--- projects/total/app.py
+++ projects/total/app.py
@@ -10,5 +10,36 @@ db = client.nabacamp
 def main():
     return render_template('index.html')
 
+@app.route('/bucket')
+def bucket():
+    return render_template('bucket.html')
+
+@app.route("/bucket/post", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form['bucket_give']
+
+    bucket_list = list(db.bucket.find({},{'_id':False}))
+    count = len(bucket_list) + 1
+
+    doc ={
+        'num':count,
+        'bucket': bucket_receive,
+        'done': 0
+    }
+    db.bucket.insert_one(doc)
+    return jsonify({'result': 'success','msg': '등록 완료!'})
+
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form['num_give']
+    db.bucket.update_one({'num': int(num_receive)},{'$set':{'done':1}})
+    return jsonify({'msg': '버킷 완료!'})
+
+@app.route("/bucket/get", methods=["GET"])
+def bucket_get():
+    bucket_list = list(db.bucket.find({},{'_id':False}))
+
+    return jsonify({'result':'success','bucket_list':bucket_list})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
