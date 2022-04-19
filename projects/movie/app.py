517289972483dbf193aa7b6c3d72b119859359d8
migdracios <migdracios@gmail.com>
2022-04-19 15:49:44 +0900

    code GET API

--- projects/movie/app.py
+++ projects/movie/app.py
@@ -43,7 +43,18 @@ def movie_post():
 
 @app.route("/movie/get", methods=["GET"])
 def movie_get():
-    return jsonify({'msg':'GET 연결 완료!'})
+    # API 설계
+    #
+    #   사용자 요청
+    #       없음
+    #   API 처리
+    #       모든 데이터베이스 찾기
+    #   데이터 응답
+    #       all_db: all_db
+
+    all_db = list(db.metaMovie.find({},{'_id':False}))
+
+    return jsonify({'result': 'success','all_db': all_db})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
