a8b1bb7f2f451341a39669162c33a615c5b047b5
migdracios <migdracios@gmail.com>
2022-04-19 11:34:59 +0900

    code quiz02 03 [dbquiz.py]

--- pythonprac/dbquiz.py
+++ pythonprac/dbquiz.py
@@ -4,9 +4,14 @@ db = client.nabacamp
 
 # 퀴즈1. 영화제목 '가버나움' 평점을 가져오기
 find = db.movies.find_one({'title':'가버나움'},{'_id':False})
-print(find)
+# print(find)
 
 # 퀴즈2. '가버나움'의 평점과 같은 영화 제목들을 가져오기
+findT = list(db.movies.find({'star':find['star']},{'_id':False}))
+for t in findT:
+    print(t['title'])
 
 # 퀴즈3. '가버나움' 영화의 평점을 문자열 0으로 만들기
+db.movies.update_one({'title':'가버나움'},{'$set':{'star':'0'}})
+print('update star')
 
