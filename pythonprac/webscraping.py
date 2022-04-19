f2b4e77a4d8f5689814aa3b1e2a10e60d3107a96
migdracios <migdracios@gmail.com>
2022-04-19 11:30:29 +0900

    web scraping DB insert with Mongo Cloud

--- pythonprac/webscraping.py
+++ pythonprac/webscraping.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.8yqbf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+db = client.nabacamp
+
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20210829',headers=headers)
 
@@ -25,4 +29,10 @@ for movie in movies:
         title = a.text
         rank = movie.select_one('td:nth-child(1) > img')['alt']
         star = movie.select_one('td.point').text
-        print(rank, title, star)
+        # print(rank, title, star)
+        doc = {
+            'title': title,
+            'rank': rank,
+            'star': star
+        }
+        db.movies.insert_one(doc)
