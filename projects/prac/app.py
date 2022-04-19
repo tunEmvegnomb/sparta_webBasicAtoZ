7796c270152d529d70ef7bbaac655548d132bf07
migdracios <migdracios@gmail.com>
2022-04-19 12:15:29 +0900

    GET API basic code

--- projects/prac/app.py
+++ projects/prac/app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,jsonify
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-   return render_template('prac.html')
+    return render_template('prac.html')
+
+
+@app.route('/test', methods=['GET'])
+def test_get():
+    title_receive = request.args.get('title_give')
+    print(title_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
