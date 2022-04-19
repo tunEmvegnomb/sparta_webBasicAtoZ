c273ae5efdc0969b873e4edc9fb4840ccf72a723
migdracios <migdracios@gmail.com>
2022-04-19 17:56:59 +0900

    insert project mars code app.py

--- projects/total/app.py
+++ projects/total/app.py
@@ -152,6 +152,55 @@ def movie_get():
     all_db = list(db.metaMovie.find({},{'_id':False}))
 
     return jsonify({'result': 'success','all_db': all_db})
+###
+
+
+# 화성땅공동구매 페이지
+@app.route('/mars')
+def mars():
+    return render_template('mars.html')
+
+
+@app.route("/mars/post", methods=["POST"])
+def web_mars_post():
+    # API 설계
+    #
+    #   사용자 요청
+    #       이름, 주소, 평수 데이터
+    #       name, address, size give
+    #   API 처리
+    #       받아온 사용자 요청값을 딕셔너리로 변수 지정정    #       데이터베이스에 저장
+    #   응답 데이터
+    #       주문이 완료되었습니다! 메시지 리턴
+    #       'result': 'success', 'msg': '주문이 완료되었습니다'
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
+    print('사용자 요청값 확인: ', name_receive, address_receive, size_receive)
+
+    doc = {
+        'name': name_receive,
+        'address': address_receive,
+        'size': size_receive
+    }
+    db.mars.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': '주문이 완료되었습니다'})
+
+
+@app.route("/mars/get", methods=["GET"])
+def web_mars_get():
+    # API 설계
+    #
+    #   사용자요청
+    #       없음
+    #   API 처리
+    #       mars DB 데이터 전체 가져오기
+    #   응답 데이터
+    #       mars_data
+
+    mars_data = list(db.mars.find({},{'_id':False}))
+
+    return jsonify({'result': 'success', 'all_data': mars_data})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
