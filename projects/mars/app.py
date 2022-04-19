d0123ff39e085441a268d25767e36db1fda63ac9
migdracios <migdracios@gmail.com>
2022-04-19 12:30:34 +0900

    API planning POST web_mars_post()

--- projects/mars/app.py
+++ projects/mars/app.py
@@ -10,9 +10,22 @@ def home():
 
 @app.route("/mars", methods=["POST"])
 def web_mars_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+    # API 설계
+    #
+    #   사용자 요청
+    #       이름, 주소, 평수 데이터
+    #       name, address, size give
+    #   API 처리
+    #       받아온 사용자 요청값을 딕셔너리로 변수 지정
+    #       데이터베이스에 저장
+    #   응답 데이터
+    #       주문이 완료되었습니다! 메시지 리턴
+    #       'result': 'success', 'msg': '주문이 완료되었습니다'
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
+    # print(sample_receive)
+    return jsonify({'result': 'success', 'msg': '주문이 완료되었습니다'})
 
 
 @app.route("/mars", methods=["GET"])
